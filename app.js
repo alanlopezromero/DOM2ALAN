ba5162f02764385f8c10afc7dfd9217cb16b8413
alanlopezromero <alopezromero192@gmail.com>
2026-03-12 11:04:34 -0600

    agrege funciones  para renderizar las miniaturas

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ const data = [
   { id: "p06", title: "Ruta", desc: "Camino en perspectiva", src: "https://picsum.photos/id/1005/1200/675" }
 ];
 
+const thumbs = document.querySelector("#thumbs"); //miniaturas
+const heroImg = document.querySelector("#heroImg");//imagen principal
+const heroTitle = document.querySelector("#heroTitle");//titulo de la imagen
+const heroDesc = document.querySelector("#heroDesc");//descripcion de la imagen
+const counter = document.querySelector("#counter");//dcontador de imagenes
+const likeBtn = document.querySelector("#likeBtn");// boton de me gusta
+
+//trabajar con el estado de la aplicacion
+let currentIndex = 0;// indice de la imagen actual
+const likes ={};//objeto para almacenar los "me gusta" por la imagen
+//renderizar las miniaturas
+function renderThumbs(){
+  thumbs.innerHTML = data.map((item,index) =>{
+    return `
+    <article class="thumb" ${index === currentIndex ? "active" : ""} data-index="${index}">
+      <span class = "badge">${index + 1}</span>
+      <img src="${item.src}" alt="${item.title}" />
+      </artitle>
+    `;
+  }).join("");
+}
+
+renderThumbs();//llamar ala funcion para mostar las miniaturas
